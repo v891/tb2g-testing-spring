2b098b8ecceda4f7e0933c54ee48e09e4c10ef0c
Vellanki, Veerendra <Veerendra_Vellanki@unigroup.com>
2020-06-28 12:45:04 -0500

    yanny test

--- src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterYannyTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterYannyTest.java
@@ -5,13 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.sfg.BaseConfig;
 import org.springframework.samples.petclinic.sfg.HearingInterpreter;
 import org.springframework.samples.petclinic.sfg.LaurelConfig;
-import org.springframework.samples.petclinic.sfg.YannyConfig;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringJUnitConfig(classes = {BaseConfig.class, YannyConfig.class})
-class HearingInterpreterLaurelTest {
+@SpringJUnitConfig(classes = {BaseConfig.class, LaurelConfig.class})
+class HearingInterpreterYannyTest {
 
     @Autowired
     HearingInterpreter hearingInterpreter;
@@ -20,6 +19,6 @@ class HearingInterpreterLaurelTest {
     void whatIheard() {
         String word = hearingInterpreter.whatIheard();
 
-        assertEquals("Yanny", word);
+        assertEquals("Laurel", word);
     }
 }
